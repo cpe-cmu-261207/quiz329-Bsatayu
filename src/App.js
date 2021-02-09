9b944552dc40c5aa3bee6153d8af0bd368133c65
Bsatayu <59277343+Bsatayu@users.noreply.github.com>
2021-02-09 14:15:48 +0700

    test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { useState, useEffect } from "react";
 
 function App() {
   //add useState for all state variables
-
+  const [myInfo, setMyInfo] = useState([]);
+  const [inputInfo, setInputInfo] = useState({ name: "", gender: "", age: "" });
   //load locationStorage
+
   useEffect(() => {
     const items = localStorage.getItem("items");
     // ...
+    if (items != null) {
+      setMyInfo(JSON.parse(items));
+    }
   }, []);
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(myInfo));
+  }, [myInfo]);
 
   return (
     <div className="card" style={{ width: 400 }}>
@@ -47,8 +55,13 @@ function App() {
         {/* display tables for all persons */}
         <p className="is-4 title has-text-centered">Person List</p>
         {/* sample table */}
-        <ItemTable name={"Bob"} gender={"Male"} age={"50"} />
-        <p>Your name and code here</p>
+        <ItemTable name={"Bob"} gender={"Male"} age={"45"} />
+        {myInfo.map((item) => {
+          return (
+            <ItemTable name={item.name} gender={item.gender} age={item.age} />
+          );
+        })}
+        <p>Satayu Settakit 620610809</p>
       </div>
     </div>
   );
